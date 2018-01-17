17d32fc025da611b32c6499d93026f9bbac6988a
vincewho <thisguyvince@gmail.com>
2018-01-17 14:55:20 -0700

    Password validation fixed!

--- PD1/solarPV_Forms.js
+++ PD1/solarPV_Forms.js
@@ -1,20 +1,20 @@
 function formValidation()
 {
 	var uname = document.registration.uname;
-	var userPassword = document.registration.userPassword;  
+	var userPassword = document.registration.userPassword;
 	var confirmPassword = document.registration.confirmPassword;
-	var zip = document.registration.zip;  
-	var email = document.registration.email;	
-	
-	if(username_validation(uname))  
-	{  
-		if(password_validation(userPassword))  
+	var zip = document.registration.zip;
+	var email = document.registration.email;
+
+	if(username_validation(uname))
+	{
+		if(password_validation(userPassword))
 		{
-			if(confirm_password(userPassword, confirmPassword))  
+			if(confirm_password(userPassword, confirmPassword))
 			{
-				if(zip_validaiton(zip))  
-				{ 
-					if(email_validation(email))  
+				if(zip_validaiton(zip))
+				{
+					if(email_validation(email))
 					{
 					}
 				}
@@ -27,19 +27,28 @@ function formValidation()
 function username_validation(uname)
 {
 	var uname_length = uname.value.length;
-	if (uname_length == 0 || uname_length > 8)
+	var unameReq = '/^[a-z0-9_-]{1,}$/';
+	uname = uname.toString();
+	if (uname_length == 0 || uname_length > 12)
 	{
-		alert("Username required! Must be at most 8 characters long.");
+		alert("Username required! Must be at most 12 characters long.");
 		uname.focus();
 		return false;
 	}
-	
-	return true;	
+	else if (uname.match(unameReq))
+	{
+		alert("This doesn't match");
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
 
 function password_validation(pw)
 {
-	var requirement = /[^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{,8}$]/;
+	var requirement = /((?=.*[a-z])(?=.*[A-Z]).{1,})/gm;
 	if (pw.value.match(requirement))
 	{
 		return true;
@@ -68,37 +77,32 @@ function confirm_password(pw, cpw)
 
 function zip_validation(zip)
 {
-	var requirement = /^[0-9]+$/;  
-	if(zip.value.match(requirement))  
-	{  
-		return true;  
-	}  
-	else  
-	{  
-		alert('ZIP code must have numeric characters only');  
-		zip.focus();  
-		return false;  
-	}  
+	var requirement = /^[0-9]+$/;
+	if(zip.value.match(requirement))
+	{
+		return true;
+	}
+	else
+	{
+		alert('ZIP code must have numeric characters only');
+		zip.focus();
+		return false;
+	}
 }
 
 function email_validation(email)
 {
-	var requirement = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
-	if(email.value.match(requirement))  
-	{  
-		alert('Form Succesfully Submitted');  
-		window.location.reload()  
-		return true;  
-	}	  
-	else  
-	{  
-		alert("You have entered an invalid email address!");  
-		email.focus();  
-		return false;  
-	} 
+	var requirement = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(email.value.match(requirement))
+	{
+		alert('Form Succesfully Submitted');
+		window.location.reload()
+		return true;
+	}
+	else
+	{
+		alert("You have entered an invalid email address!");
+		email.focus();
+		return false;
+	}
 }
-
-
-
-
-
