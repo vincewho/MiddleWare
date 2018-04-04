0c99c20018c3c5849e66411082b2d455979b82e3
vincewho <thisguyvince@gmail.com>
2018-04-03 22:49:02 -0700

    uploaded pd6 finished

---
+++ PD6/solarProject/solarpv/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+  return render(request, 'solarpv/solarPV_Home.html')
+
+def regU(request):
+  return render(request, 'solarpv/solarPV_Register.html')
+  
+def regM(request):
+  return render(request, 'solarpv/solarPV_RegisterModule.html')
+    
+def testp(request):
+  return render(request, 'solarpv/solarPV_TestingPage.html')
+  
+def rating(request):
+  return render(request, 'solarpv/solarPV_RatingPage.html')
